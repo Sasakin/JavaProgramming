0d4d1d9b7dc009fc9e3b4331cb41f5327826e39c
sasakin <max_to_day@mail.ru>
2017-08-21 17:26:25 +0300

    Create ThreadTimedWaitingStateTest

---
+++ src/Chapter14/ThreadTimedWaitingStateTast.java
@@ -0,0 +1,34 @@
+package Chapter14;
+
+/**
+ * Created by sasakin on 21.08.2017.
+ */
+// состояния NEW, RUNNABLE, TIMED_WAITING, TERMINATED
+public class ThreadTimedWaitingStateTast extends Thread {
+    public void run() {
+        try {
+            Thread.sleep(50);
+        } catch (InterruptedException e) {
+            System.err.println("ошибка потока");
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            Thread thread = new ThreadTimedWaitingStateTast();
+            System.out.println("1: " + thread.getState());
+            thread.start();
+            //RUNNABLE - поток запущен
+            System.out.println("2: " + thread.getState());
+            Thread.sleep(10);
+            // TIMED_WAITING
+            // поток ждет некоторое время окончания работы другого потока
+            System.out.println("3: " + thread.getState());
+            thread.join();
+            //TERMINATED - поток завершил выполнение
+            System.out.println("4: "+ thread.getState());
+        } catch (InterruptedException e) {
+            System.err.println("ошибка потока");
+        }
+    }
+}
